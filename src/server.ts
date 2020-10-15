d6329fe93d387b17e0f644c50c2aaf9acbb6483d
Lucas Cunha <lucasviana112@gmail.com>
2020-10-14 22:34:02 -0300

    created route for create new orphanage (POST)

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,45 @@
 import express from 'express'
+import {getRepository} from 'typeorm'
+import Orphanage from './models/Orphanage'
+
+import './database/connection'
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ ok: true })
+app.use(express.json());
+
+// app.get('/', (request, response) => {
+//     console.log(request.query);
+//     console.log(request.params);
+//     console.log(request.body);
+//     return response.json({ ok: true })
+// })
+
+app.post('/orphanages', async (request, response) => {
+    console.log(request.body)
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+    await orphanagesRepository.save(orphanage);
+    
+    return response.status(201).json(orphanage)
 })
 
 app.listen(3333);
